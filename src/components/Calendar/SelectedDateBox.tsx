6dfa461eed913867e930b1a6eee5ebc53e51e6a1
aneomagig <imapversion4@gmail.com>
2025-03-25 17:18:37 +0900

    feat: SelectedDateBox 컴포넌트 제작

--- src/components/Calendar/SelectedDateBox.tsx
+++ src/components/Calendar/SelectedDateBox.tsx
@@ -10,7 +10,7 @@ const SelectedDateBox: React.FC<SelectedDateBoxProps> = ({ date, label }) => {
   const iconName = date ? 'dateselect_left_blue' : 'dateselect_left_black';
 
   return (
-    <div className='inline-flex bg-white bg-opacity-60 border-mono_e gap-2 px-4 py-3 rounded-xl items-center w-full'>
+    <div className='inline-flex bg-white bg-opacity-60  border border-mono_e gap-2 px-4 py-3 rounded-xl items-center w-full'>
       <Icon name={iconName} size={24} />
       {date ? (
         <p className='text-blue_c text-base font-medium'>
