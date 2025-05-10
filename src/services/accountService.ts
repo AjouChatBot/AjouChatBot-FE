a0d43200bddb21b948c31d3c59b61a7afe850c22
kyo <90518589+aneomagig@users.noreply.github.com>
2025-05-11 00:19:45 +0900

    feat: 계정정보 가져올 때 apiClient 사용해서 fetch하도록 수정

--- src/services/accountService.ts
+++ src/services/accountService.ts
@@ -1,15 +1,7 @@
+import apiClient from './apiClient';
 import { AccountResponse } from '../types/account';
 
-const API_URL = import.meta.env.VITE_APP_API_URL;
-
 export const getAccountInfo = async (): Promise<AccountResponse> => {
-  const response = await fetch(`${API_URL}/account/info`, {
-    credentials: 'include',
-  });
-
-  if (!response.ok) {
-    throw new Error('Failed to fetch account info');
-  }
-
-  return response.json();
+  const response = await apiClient.get<AccountResponse>('/account/info');
+  return response.data;
 };
