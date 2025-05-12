68f8fbd9d21e682ba7cc8b2d69a1855cc4eafd4e
kyo <90518589+aneomagig@users.noreply.github.com>
2025-05-12 14:31:13 +0900

    feat: 세팅 페이지 완성

--- src/pages/SettingAccount.tsx
+++ src/pages/SettingAccount.tsx
@@ -4,11 +4,15 @@ import SettingLine from '../components/setting/SettingLine';
 import SettingProfile from '../components/setting/SettingProfile';
 import SettingSidebar from '../components/setting/SettingSidebar';
 import SettingTitle from '../components/setting/SettingTitle';
+import { useUser } from '../contexts/UserContext';
 
 const SettingAccount = () => {
+  const { user } = useUser();
+  if (!user) return null;
+
   return (
     <Layout>
-      <div className='flex w-full gap-[116px] ml-[65px]'>
+      <div className='flex w-full gap-[116px] ml-[65px] mt-[60px] mb-[60px]'>
         <SettingSidebar />
         <div className='w-full mr-[206px]'>
           <div className='mb-9'>
@@ -20,16 +24,16 @@ const SettingAccount = () => {
           <div className='w-full flex gap-[60px]'>
             <div className='w-full flex flex-col gap-6'>
               <SettingLine text='개인정보' />
-              <SettingDetail label='이름' value='ㅁㅁㅁ' />
-              <SettingDetail label='전화번호' value='010-1234-5678' />
-              <SettingDetail label='이름' value='abc@ajou.ac.kr' />
+              <SettingDetail label='이름' value={user.name} />
+              <SettingDetail label='전화번호' value={user.phone} />
+              <SettingDetail label='이메일' value={user.email} />
             </div>
             <div className='w-full flex flex-col gap-6'>
               <SettingLine text='재학정보' />
-              <SettingDetail label='단과대학' value='ㅁㅁㅁ' />
-              <SettingDetail label='소속학과' value='ㅁㅁㅁ' />
-              <SettingDetail label='제1전공' value='ㅁㅁㅁ' />
-              <SettingDetail label='학년' value='ㅁㅁㅁ' />
+              <SettingDetail label='단과대학' value={user.college} />
+              <SettingDetail label='소속학과' value={user.department} />
+              <SettingDetail label='제1전공' value={user.major} />
+              <SettingDetail label='학년' value={`${user.grade}학년`} />
             </div>
           </div>
         </div>
--- src/pages/SettingChat.tsx
+++ src/pages/SettingChat.tsx
@@ -1,10 +1,43 @@
+import { useState } from 'react';
 import Layout from '../components/layout/Layout';
+import SettingBlock from '../components/setting/SettingBlock';
 import SettingSidebar from '../components/setting/SettingSidebar';
+import SettingTitle from '../components/setting/SettingTitle';
 
 const SettingChat = () => {
+  const [toggleStates, setToggleStates] = useState({
+    grade: false,
+  });
+
+  const handleToggle = (key: keyof typeof toggleStates) => {
+    setToggleStates((prev) => ({
+      ...prev,
+      [key]: !prev[key],
+    }));
+  };
+
   return (
     <Layout>
-      <SettingSidebar />
+      <div className='w-full flex gap-[116px] ml-[65px] mt-[60px] mb-[60px]'>
+        <SettingSidebar />
+        <div className='w-full mr-[160px]'>
+          <SettingTitle type='manageCollectedData' />
+          <div className='w-full flex flex-col gap-2.5 mt-9'>
+            <SettingBlock
+              title='성적정보'
+              description='수강과목에 대한 성적정보'
+              toggle
+              isOn={toggleStates.grade}
+              onToggle={() => handleToggle('grade')}
+            />
+            <SettingBlock
+              title='사용자 맞춤데이터 수집'
+              arrow={true}
+              danger={true}
+            />
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
