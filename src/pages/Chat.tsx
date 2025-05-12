1b47e0abf0d67fede010d295cfd6509d5bae8edd
kyo <90518589+aneomagig@users.noreply.github.com>
2025-05-12 14:31:44 +0900

    style: 채팅창, 채팅 로그 스타일 변경

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import ChatInput from '../components/chat/ChatInput';
 import { useChat } from '../contexts/ChatContext';
 import Layout from '../components/layout/Layout';
@@ -6,14 +6,17 @@ import TalkArea from '../components/chat/TalkArea';
 
 const ChatPage: React.FC = () => {
   const { chatLogs, isBotTyping, handleSend, botMessage } = useChat();
+  const bottomRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [chatLogs, isBotTyping]);
 
   return (
     <Layout>
-      {/* 전체 영역을 정확히 분할 */}
-      <div className='flex flex-col w-screen h-screen'>
-        {/* 메시지 영역 (스크롤 가능) */}
-        <div className='flex-1 overflow-y-auto px-8 pt-6 pb-2'>
-          <div className='w-full max-w-[1200px] mx-auto flex flex-col gap-4'>
+      <div className='flex flex-col w-full min-h-[calc(100vh-80px)]'>
+        <div className='absolute top-0 bottom-[400px] left-0 right-0 overflow-y-auto px-8 pt-6 pb-4 scrollbar-hide'>
+          <div className='w-full max-w-[1200px] mx-auto flex flex-col gap-4 '>
             {chatLogs.map((log, index) => (
               <div
                 key={index}
@@ -35,12 +38,11 @@ const ChatPage: React.FC = () => {
                 />
               </div>
             )}
+            <div ref={bottomRef} />
           </div>
         </div>
-
-        {/* 입력창 (시각적으로 띄우기 + 높이 차지) */}
-        <div className='w-full px-8 pb-60 pt-4'>
-          <div className='max-w-[1200px] mx-auto'>
+        <div className='absolute bottom-20 left-0 right-0 px-8 py-4 justify-center'>
+          <div className='max-w-[1200px] mx-auto justify-center'>
             <ChatInput mode='chat' onSend={handleSend} />
           </div>
         </div>
