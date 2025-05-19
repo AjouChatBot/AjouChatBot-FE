2726efed50a6435ccd631fb2e45bfe52df4dbba9
kyo <90518589+aneomagig@users.noreply.github.com>
2025-05-19 23:46:40 +0900

    feat: 채팅 설정 api

--- src/pages/SettingChat.tsx
+++ src/pages/SettingChat.tsx
@@ -3,6 +3,7 @@ import Layout from '../components/layout/Layout';
 import SettingBlock from '../components/setting/SettingBlock';
 import SettingSidebar from '../components/setting/SettingSidebar';
 import SettingTitle from '../components/setting/SettingTitle';
+import { deleteCustomUserData } from '../services/collectedDataService';
 
 const SettingChat = () => {
   const [toggleStates, setToggleStates] = useState({
@@ -16,6 +17,20 @@ const SettingChat = () => {
     }));
   };
 
+  const handleDeleteData = async () => {
+    try {
+      const res = await deleteCustomUserData();
+      if (res.status === 'success') {
+        alert(res.message);
+      } else {
+        alert(res.message);
+      }
+    } catch (error) {
+      alert('초기화 요청 중 오류가 발생했습니다.');
+      console.error(error);
+    }
+  };
+
   return (
     <Layout>
       <div className='w-full flex gap-[116px] ml-[65px] mt-[60px] mb-[60px]'>
@@ -24,16 +39,16 @@ const SettingChat = () => {
           <SettingTitle type='manageCollectedData' />
           <div className='w-full flex flex-col gap-2.5 mt-9'>
             <SettingBlock
-              title='성적정보'
-              description='수강과목에 대한 성적정보'
+              title='사용자 맞춤데이터 수집'
               toggle
               isOn={toggleStates.grade}
               onToggle={() => handleToggle('grade')}
             />
             <SettingBlock
-              title='사용자 맞춤데이터 수집'
+              title='A.mate 맞춤형 수집데이터 초기화'
               arrow={true}
               danger={true}
+              onClick={handleDeleteData}
             />
           </div>
         </div>
