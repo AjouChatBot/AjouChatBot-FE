687415e9a341f059755ea14496ed5448a104bee0
kyo <90518589+aneomagig@users.noreply.github.com>
2025-05-20 23:34:54 +0900

    style: 스타일 변경

--- src/components/chat/TalkArea.tsx
+++ src/components/chat/TalkArea.tsx
@@ -5,33 +5,30 @@ interface TalkAreaProps {
   direction?: 'left' | 'right';
   status?: 'inputted' | 'pending' | 'error';
 }
-
 const TalkArea: React.FC<TalkAreaProps> = ({
   message,
   direction = 'left',
   status = 'inputted',
 }) => {
-  const getBubbleStyle = () => {
-    const baseStyle =
-      'inline-block px-5 py-3 bg-white shadow-[0px_0px_3px_0px_rgba(3,100,179,0.25)]';
-    let directionStyle = '';
-
-    if (direction === 'left') {
-      directionStyle = 'rounded-tr-xl rounded-bl-xl rounded-br-xl';
-    } else {
-      directionStyle = 'rounded-tl-xl rounded-bl-xl rounded-br-xl';
-    }
-
-    return `${baseStyle} ${directionStyle}`;
-  };
+  const isUser = direction === 'right';
 
   return (
-    <div
-      data-status={status}
-      data-talk_direction={direction}
-      className={getBubbleStyle()}
-    >
-      <div className='text-base leading-loose'>{message}</div>
+    <div className={`flex ${isUser ? 'justify-end' : 'justify-start'}`}>
+      <div
+        data-status={status}
+        data-talk_direction={direction}
+        className={`
+          px-5 py-3 bg-white shadow-[0px_0px_3px_0px_rgba(3,100,179,0.25)]
+          ${
+            isUser
+              ? 'rounded-tl-xl rounded-bl-xl rounded-br-xl bg-white'
+              : 'rounded-tr-xl rounded-bl-xl rounded-br-xl bg-white'
+          }
+          max-w-[75%] break-words whitespace-pre-wrap
+        `}
+      >
+        <div className='text-base leading-loose'>{message}</div>
+      </div>
     </div>
   );
 };
