df5d0a7177569b4c46743f444d543fee7ba2ebaa
kyo <90518589+aneomagig@users.noreply.github.com>
2025-05-27 23:36:18 +0900

    feat: 새로운 페이지 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation, Navigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import Home from './pages/Home';
 import Chat from './pages/Chat';
 import Login from './pages/Login';
@@ -7,19 +8,47 @@ import { UserProvider } from './contexts/UserContext';
 import SettingAccount from './pages/SettingAccount';
 import SettingAcademic from './pages/SettingAcademic';
 import SettingChat from './pages/SettingChat';
+import MobileBoarding from './components/MobileBoarding';
+import MobileLogin from './pages/MobileLogin';
+import MobileHome from './pages/MobileHome';
+import MobileChat from './pages/MobileChat';
 
 const App = () => {
+  const location = useLocation();
+  const [isLoading, setIsLoading] = useState(true);
+  const isMobilePath = location.pathname.startsWith('/m');
+
+  useEffect(() => {
+    if (isMobilePath) {
+      setIsLoading(true);
+      const timer = setTimeout(() => {
+        setIsLoading(false);
+      }, 0);
+
+      return () => clearTimeout(timer);
+    }
+  }, [location.pathname, isMobilePath]);
+
+  if (isMobilePath && isLoading) {
+    return <MobileBoarding />;
+  }
+
   return (
     <UserProvider>
       <ChatProvider>
         <Routes>
-          <Route path='/' element={<Home/>}/>
+          <Route path='/' element={<Home />} />
           <Route path='/home' element={<Home />} />
           <Route path='/chat' element={<Chat />} />
           <Route path='/login' element={<Login />} />
           <Route path='/setting/account' element={<SettingAccount />} />
           <Route path='/setting/academic' element={<SettingAcademic />} />
           <Route path='/setting/chat' element={<SettingChat />} />
+
+          <Route path='/m/home' element={<MobileHome />} />
+          <Route path='/m/chat' element={<MobileChat />} />
+          <Route path='/m/login' element={<MobileLogin />} />
+          <Route path='/m/*' element={<Navigate to='/m/home' replace />} />
         </Routes>
       </ChatProvider>
     </UserProvider>
--- src/components/constants/PageTitles.ts
+++ src/components/constants/PageTitles.ts
@@ -3,6 +3,10 @@ const pageTitles: Record<string, string> = {
   '/chat': 'Chat',
   '/login': 'Login',
   '/setting': 'Setting',
+  '/m/boarding': 'Boarding',
+  '/m/home': 'Home',
+  '/m/chat': 'Chat',
+  '/m/login': 'Login',
 };
 
 export default pageTitles;
