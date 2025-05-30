f03553d9cf537b04e990a9fbea6d900b9abfa731
kyo <90518589+aneomagig@users.noreply.github.com>
2025-05-30 10:30:43 +0900

    feat: 키보드 인식 기능 도입

---
+++ src/utils/keyboardScrollHandler.ts
@@ -0,0 +1,92 @@
+interface KeyboardScrollOptions {
+  onFocus?: () => void;
+  onBlur?: () => void;
+}
+
+let isFocus = false;
+let scrollElement: HTMLDivElement | null = null;
+
+const appendScrollElement = () => {
+  if (!scrollElement) {
+    scrollElement = document.createElement('div');
+    scrollElement.id = 'make-scrollable';
+    scrollElement.style.cssText = `
+      position: absolute;
+      left: 0;
+      width: 1px;
+      height: 0;
+      transition: height 0.5s linear;
+    `;
+    document.body.appendChild(scrollElement);
+  }
+};
+
+const removeScrollElement = () => {
+  if (scrollElement && scrollElement.parentNode) {
+    scrollElement.parentNode.removeChild(scrollElement);
+    scrollElement = null;
+  }
+};
+
+const handleFocus = () => {
+  isFocus = true;
+  appendScrollElement();
+  if (scrollElement) {
+    scrollElement.style.height = 'calc(100% + 1px)';
+  }
+  document.body.style.overflow = 'hidden';
+};
+
+const handleBlur = () => {
+  isFocus = false;
+  if (scrollElement) {
+    scrollElement.style.height = '0';
+  }
+  document.body.style.overflow = '';
+};
+
+const triggerKeyboardOpen = () => {
+  if (isFocus) {
+    handleFocus();
+  } else {
+    handleBlur();
+  }
+};
+
+export const setupKeyboardScrollHandler = (
+  options: KeyboardScrollOptions = {}
+) => {
+  const { onFocus, onBlur } = options;
+
+  const handleViewportResize = () => {
+    triggerKeyboardOpen();
+  };
+
+  const handleInputFocus = (e: FocusEvent) => {
+    handleFocus();
+    onFocus?.();
+  };
+
+  const handleInputBlur = () => {
+    handleBlur();
+    onBlur?.();
+  };
+
+  if (window.visualViewport) {
+    window.visualViewport.addEventListener('resize', handleViewportResize);
+  }
+
+  return {
+    handleInputFocus,
+    handleInputBlur,
+    cleanup: () => {
+      if (window.visualViewport) {
+        window.visualViewport.removeEventListener(
+          'resize',
+          handleViewportResize
+        );
+      }
+      removeScrollElement();
+    },
+  };
+};
